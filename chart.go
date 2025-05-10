793cda6e696616db336a71a8651a476f6f98a20a
wiktrek <me@example.com>
2025-05-10 13:50:50 +0200

    .

--- chart.go
+++ chart.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("hello world");
-	fmt.Println("hello world");
+	generate_columns(4, "column 1, column 2, column 3, column 4", "10,12,14,16");
+}
+	func generate_columns(amount int, column_names string, values string) {
+		names := strings.Split(column_names, ",")
+		v := strings.Split(values, ",")
+		for i := 0; i < amount; i++ {
+			fmt.Println(names[i])
+			fmt.Println(v[i])
+		}
 }
---
+++ readme.md
@@ -0,0 +1,1 @@
+# display simple charts in console
